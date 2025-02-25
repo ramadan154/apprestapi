fbe7a213b23d4bf7f6cd22fc2c232da87aa9ee5c
ramadan154 <ramadanhufazd@gmail.com>
2025-02-26 01:27:11 +0700

    berhasil menampilkan semua data dan semua data berdasarkan id

--- controller.js
+++ controller.js
@@ -6,3 +6,27 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("Aplikasi REST API ku berjalan!",res)
 };
+
+//menampilkan semua data
+exports.tampilsemuadata = function(req,res) {
+    connection.query('SELECT * FROM pengguna', function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
+};
+
+//menampilkan semua data berdasarkan id
+exports.tampildataid = function(req, res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM pengguna WHERE id_pengguna = ?', [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok(rows, res);
+            }
+        });
+};

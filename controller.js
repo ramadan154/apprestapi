7fc789998e5b1b3cf1b09894196c430adf414d57
ramadan154 <ramadanhufazd@gmail.com>
2025-02-26 09:27:19 +0700

    update controller

--- controller.js
+++ controller.js
@@ -3,29 +3,29 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req,res){
-    response.ok("Aplikasi REST API ku berjalan!",res)
+exports.index = function (req, res) {
+    response.ok("Aplikasi REST API ku berjalan!", res)
 };
 
 //menampilkan semua data
-exports.tampilsemuadata = function(req,res) {
-    connection.query('SELECT * FROM pengguna', function(error, rows, fields){
-        if(error){
+exports.tampilsemuadata = function (req, res) {
+    connection.query('SELECT * FROM pengguna', function (error, rows, fields) {
+        if (error) {
             console.log(error);
-        }else{
+        } else {
             response.ok(rows, res);
         }
     });
 };
 
 //menampilkan semua data berdasarkan id
-exports.tampildataid = function(req, res){
+exports.tampildataid = function (req, res) {
     let id = req.params.id;
     connection.query('SELECT * FROM pengguna WHERE id_pengguna = ?', [id],
-        function(error, rows, fields){
-            if(error){
+        function (error, rows, fields) {
+            if (error) {
                 console.log(error);
-            }else{
+            } else {
                 response.ok(rows, res);
             }
         });
